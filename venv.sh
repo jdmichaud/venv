da0762bb24c39ab4c535cf532435013507429567
jd <jean.daniel.michaud@gmail.com>
2019-02-09 14:37:06 +0100

    Make venv executable

--- venv.sh
+++ venv.sh
@@ -1,5 +1,19 @@
 #/bin/sh
 
+function usage() {
+  echo "usage: ./venv.sh <folder>"
+  echo "Create a virtual environment in the provided folder if it does not already exists."
+  echo "Then chroot into that environment and span a bash process."
+}
+
+if [[ $# -ne 2 ]]
+then
+  usage
+  exit 1
+fi
+exit 0
+
+
 mkdir .env
 mkdir .env/{upper,work,mount}
 sudo mount -t overlay -o lowerdir=/,upperdir=.env/upper,workdir=.env/work overlayfs .env/mount
